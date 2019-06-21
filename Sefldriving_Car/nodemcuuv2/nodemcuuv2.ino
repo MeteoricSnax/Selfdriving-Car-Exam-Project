8ac153f6df2f463cc4ba53bdb423aed1d053720e
Jonas Elbro <cph-je98@cphbusiness.dk>
2019-06-21 11:31:12 +0200

    add arduino and nodemcu

---
+++ Sefldriving_Car/nodemcuuv2/nodemcuuv2.ino
@@ -0,0 +1,103 @@
+//// Servo setup
+#include <Servo.h>
+Servo servo;
+
+int posright = 110; //Postion for front wheels when turning right
+int posleft = 50;   //Postion for front wheels when turning left
+int posstraight = 80; //Postion for front wheels when going straight forward
+
+int servopin = D7; //Attached servo on pin D7
+int dangerpin = D8; // Danger signal pin D8 fron Arduino UNO
+
+// Motor setup
+int motorPin = D6;
+int motorDirection = 0; //Motor direction
+int motorSpeed = 5; // Motor speed
+
+int state = 1;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.flush();
+  // Pin setup
+  pinMode(motorPin, OUTPUT);
+  pinMode(motorDirection, OUTPUT);
+  pinMode(motorSpeed, OUTPUT);
+  pinMode(dangerpin, INPUT);
+
+  //Adjust speed of motors
+  analogWrite(motorSpeed, 900); // Max is 1040
+
+  go(); // All systems go
+}
+
+void loop() {
+  if (digitalRead(dangerpin) == HIGH) { // Initiate danger sequence
+    reverse();
+    delay(800);
+
+    brake();
+    left();
+    delay(500);
+
+    reverse();
+    delay(800);
+
+    brake();
+    right();
+    delay(500);
+
+    go();
+    delay(800);
+    
+    brake();
+    straight();
+    delay(500);
+
+    state = 1;
+  } else if (state == 1) {
+    go();
+    state = 0;
+  } else {
+    return;
+  }
+}
+
+
+void brake() {
+  digitalWrite(motorPin, HIGH);
+}
+
+void go() {
+  digitalWrite(motorPin, LOW);
+  digitalWrite(motorDirection, HIGH);
+}
+
+void reverse() {
+  digitalWrite(motorPin, LOW);
+  digitalWrite(motorDirection, LOW);
+}
+
+void left() {
+  servo.attach(servopin);
+  delay(300);
+  servo.write(posleft); // Position for front wheels
+  delay(600);
+  servo.detach();
+}
+
+void right() {
+  servo.attach(servopin);
+  delay(300);
+  servo.write(posright); // Position for front wheels
+  delay(600);
+  servo.detach();
+}
+
+void straight() {
+  servo.attach(servopin);
+  delay(300);
+  servo.write(posstraight); // Position for front wheels
+  delay(600);
+  servo.detach();
+}
